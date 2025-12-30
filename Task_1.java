cf82e7c9d2f1953efb9eb85ff86be9985523a566
ABID-ALI-07 <vtu24497@veltech.edu.in>
2025-12-30 20:47:49 +0530

    Task_1.java

--- Task_1.java
+++ Task_1.java
@@ -1,19 +1,23 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        int left = 0;
-        int right = n - 1;
-        for (int i = n - 1; i >= 0; i--) {
-            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
-                result[i] = nums[left] * nums[left];
+        int n=nums.length;
+        int[]result=new int[n];
+        int left=0;
+        int right=n-1;
+        int pos=n-1;
+        while(left<=right){
+            int leftsquare=nums[left]*nums[left];
+            int rightsquare=nums[right]*nums[right];
+            if(leftsquare>rightsquare){
+                result[pos]=leftsquare;
                 left++;
-            } else {
-                result[i] = nums[right] * nums[right];
+            }
+            else{
+                result[pos]=rightsquare;
                 right--;
             }
-        }
-        
-        return result;
+               pos--;
+        }    
+     return result;
     }
 }
