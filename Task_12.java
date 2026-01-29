581b78989585d24669e6be7d8427b56a840e079e
ABID-ALI-07 <vtu24497@veltech.edu.in>
2026-01-29 10:19:43 +0530

    Create Task_12.java

---
+++ Task_12.java
@@ -0,0 +1,38 @@
+class Task_12{
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        // 1. Find the middle of the linked list
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // 2. Reverse the second half of the list
+        ListNode secondHalf = reverse(slow);
+        ListNode firstHalf = head;
+
+        // 3. Compare the two halves
+        ListNode temp = secondHalf; // To restore later if needed
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val) return false;
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
